cdc864f7f0a48044cde033a55d6a7f84efbf5ff7
FredNekrasov <frednekrasov49@gmail.com>
2025-03-23 19:58:58 +0300

    создание api модуля для авторизации и класса шифрования данных

---
+++ features/auth/api/src/main/java/com/fredprojects/knyazhich/features/auth/api/Encryptor.kt
@@ -0,0 +1,13 @@
+package com.fredprojects.knyazhich.features.auth.api
+
+/** Класс, содержащий логику шифрования данных **/
+class Encryptor {
+    /**
+     * Метод шифрования данных
+     * @param data данные, которые нужно зашифровать
+     * @return зашифрованные данные
+     */
+    fun encrypt(data: String): String {
+        return data// пока так, дальше реализуем алгоритм шифрования
+    }
+}
